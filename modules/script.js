1ab0b9b408a104a2d82962b04e96e8adcbf90239
Erik Vold <erikvvold@gmail.com>
2010-06-30 15:15:34 -0700

    Moving the script parse method's guts to modules/script.js from modules/config.js, because it requires access to so many private members. The parse method is not part of the prototype because I thought that only a singleton is needed in this case.

--- modules/script.js
+++ modules/script.js
@@ -7,6 +7,8 @@ Cu.import("resource://greasemonkey/utils.js");
 Cu.import("resource://greasemonkey/miscapis.js");
 Cu.import("resource://greasemonkey/convert2RegExp.js");
 Cu.import("resource://greasemonkey/scriptdownloader.js");
+Cu.import("resource://greasemonkey/scriptrequire.js");
+Cu.import("resource://greasemonkey/scriptresource.js");
 
 GM_apiAcceptableFile(Components.stack.filename);
 
@@ -289,4 +291,128 @@ Script.prototype = {
     this._modified = this._file.lastModifiedTime;
     this._metahash = GM_sha1(this._rawMeta);
   }
+};
+
+Script.parse = function parse(config, source, uri, updating) {
+  var script = new Script(config);
+
+  if (uri) {
+    script._downloadURL = uri.spec;
+    script._enabled = true;
+  }
+
+  // read one line at a time looking for start meta delimiter or EOF
+  var lines = source.match(/.+/g);
+  var lnIdx = 0;
+  var result = {};
+  var foundMeta = false;
+
+  while ((result = lines[lnIdx++])) {
+    if (result.indexOf("// ==UserScript==") == 0) {
+      foundMeta = true;
+      break;
+    }
+  }
+
+  // gather up meta lines
+  if (foundMeta) {
+    // used for duplicate resource name detection
+    var previousResourceNames = {};
+    script._rawMeta = "";
+
+    while ((result = lines[lnIdx++])) {
+      if (result.indexOf("// ==/UserScript==") == 0) {
+        break;
+      }
+
+      var match = result.match(/\/\/ \@(\S+)(?:\s+([^\n]+))?/);
+      if (match === null) continue;
+
+      var header = match[1];
+      var value = match[2];
+
+      if (!value) {
+        switch (header) {
+          case "unwrap":
+            script._unwrap = true;
+            break;
+          default:
+            continue;
+        }
+      }
+
+      switch (header) {
+        case "name":
+        case "namespace":
+        case "description":
+        case "version":
+          script["_" + header] = value;
+          break;
+        case "include":
+          script.addInclude(value);
+          break;
+        case "exclude":
+          script.addExclude(value);
+          break;
+        case "require":
+          try {
+            var reqUri = GM_uriFromUrl(value, uri);
+            var scriptRequire = new ScriptRequire(script);
+            scriptRequire._downloadURL = reqUri.spec;
+            script._requires.push(scriptRequire);
+            script._rawMeta += header + '\0' + value + '\0';
+          } catch (e) {
+            if (updating) {
+              script._dependFail = true;
+            } else {
+              throw new Error('Failed to @require '+ value);
+            }
+          }
+          break;
+        case "resource":
+          var res = value.match(/(\S+)\s+(.*)/);
+          if (res === null) {
+            // NOTE: Unlocalized strings
+            throw new Error("Invalid syntax for @resource declaration '" +
+                            value + "'. Resources are declared like: " +
+                            "@resource <name> <url>.");
+          }
+
+          var resName = res[1];
+          if (previousResourceNames[resName]) {
+            throw new Error("Duplicate resource name '" + resName + "' " +
+                            "detected. Each resource must have a unique " +
+                            "name.");
+          } else {
+            previousResourceNames[resName] = true;
+          }
+
+          try {
+            var resUri = GM_uriFromUrl(res[2], uri);
+            var scriptResource = new ScriptResource(script);
+            scriptResource._name = resName;
+            scriptResource._downloadURL = resUri.spec;
+            script._resources.push(scriptResource);
+            script._rawMeta += header + '\0' + resName + '\0' + resUri.spec + '\0';
+          } catch (e) {
+            if (updating) {
+              script._dependFail = true;
+            } else {
+              throw new Error('Failed to get @resource '+ resName +' from '+
+                              res[2]);
+            }
+          }
+          break;
+      }
+    }
+  }
+
+  // if no meta info, default to reasonable values
+  if (!script._name && uri) script._name = GM_parseScriptName(uri);
+  if (!script._namespace && uri) script._namespace = uri.host;
+  if (!script._description) script._description = "";
+  if (!script._version) script._version = "";
+  if (script._includes.length == 0) script.addInclude("*");
+
+  return script;
 };
