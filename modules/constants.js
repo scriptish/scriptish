045e18c3ce4b076b5eafebab09487350404b9220
Erik Vold <erikvvold@gmail.com>
2010-07-15 22:25:02 -0700

    moving GM_firefoxVersion to it's own file.

--- modules/constants.js
+++ modules/constants.js
@@ -4,8 +4,7 @@ var EXPORTED_SYMBOLS = [
     "Ci",
     "gmService",
     "ioService",
-    "GM_os",
-    "GM_firefoxVersion"];
+    "GM_os"];
 
 const Cc = Components.classes;
 const Ci = Components.interfaces;
@@ -23,24 +22,4 @@ function getOS() {
   getOS = function() { return os; };
 
   return os;
-}
-
-var GM_firefoxVersion = function() { return getFFVersion(); }
-function getFFVersion() {
-  var version = (function() {
-    var version = Cc["@mozilla.org/xre/app-info;1"]  
-        .getService(Ci.nsIXULAppInfo).version;
-    var versionChecker = Cc["@mozilla.org/xpcom/version-comparator;1"]
-        .getService(Ci.nsIVersionComparator);
-
-    // Detect fixed possible compatible Firefox versions.
-    if (versionChecker.compare(version, '3.5') < 0) return '3.0';
-    else if (versionChecker.compare(version, '3.6') < 0) return '3.5';
-    else if (versionChecker.compare(version, '3.7') < 0) return '3.6';
-    else return '4.0';
-  })();
-
-  getFFVersion = function() { return version; }
-
-  return version
 }
