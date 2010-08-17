414a2e4026e81380430e1dd6b253bdd44895e0af
Erik Vold <erikvvold@gmail.com>
2010-08-17 02:06:07 -0700

    uninstalling userscripts when the addons tab manager is closed.

--- content/addonstab.js
+++ content/addonstab.js
@@ -1,5 +1,7 @@
 
-Components.utils.import("resource://scriptish/addonprovider.js");
+Cu.import("resource://scriptish/addonprovider.js");
+
+(function() {
 
 function GM_ExtendedStringBundle(aBase) {
   this.basebundle = aBase;
@@ -24,3 +26,17 @@ GM_ExtendedStringBundle.prototype = {
 var GM_bundle = new GM_ExtendedStringBundle(window.gStrings.ext);
 GM_bundle.strings["header-userscript"] = "User Scripts";
 window.gStrings.ext = GM_bundle;
+
+})();
+
+window.addEventListener("unload", function() {
+  var tools = {};
+  Cu.import("resource://scriptish/constants.js", tools);
+
+  var config = tools.gmService.config;
+  var scripts = config.scripts;
+
+  for (var i = scripts.length - 1; i >= 0; i--) {
+    if (scripts[i].needsUninstall) config.uninstall(scripts[i]);
+  }
+}, false);
