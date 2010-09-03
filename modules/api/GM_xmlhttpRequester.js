809f6110584558485ce4c38be27d3c594e49090b
Chris Williams <chris@dept42.com>
2010-09-03 08:19:09 +0800

    Add pass-through for mozBackgroundRequest on GM_XHR

--- modules/api/GM_xmlhttpRequester.js
+++ modules/api/GM_xmlhttpRequester.js
@@ -68,6 +68,8 @@ function(safeUrl, details, req) {
   this.setupRequestEvent(this.unsafeContentWin, req, "onreadystatechange",
                          details);
 
+  if (details.mozBackgroundRequest) { req.mozBackgroundRequest = true; }
+
   req.open(details.method, safeUrl);
 
   if (details.overrideMimeType) {
