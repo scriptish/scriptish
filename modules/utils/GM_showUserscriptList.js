fb714fe20c952f4bfb9e1d62672f1d2f168233b6
Erik Vold <erikvvold@gmail.com>
2010-09-05 23:47:47 -0700

    renaming GM_firefoxVersion to Scriptish_firefoxVersion

--- modules/utils/GM_showUserscriptList.js
+++ modules/utils/GM_showUserscriptList.js
@@ -4,12 +4,11 @@ var EXPORTED_SYMBOLS = ["GM_showUserscriptList"];
 
 const Cu = Components.utils;
 Cu.import("resource://scriptish/constants.js");
-Cu.import("resource://scriptish/utils/GM_firefoxVersion.js");
 
 const GM_showUserscriptList = function() {
   var chromeWin = Cc['@mozilla.org/appshell/window-mediator;1']
       .getService(Ci.nsIWindowMediator)
       .getMostRecentWindow("navigator:browser");
-  chromeWin.BrowserOpenAddonsMgr(
-      GM_firefoxVersion == '4.0' ? 'addons://list/userscripts' : 'userscripts');
+
+  chromeWin.BrowserOpenAddonsMgr('addons://list/userscripts');
 }
