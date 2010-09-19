246b7e2567af6d28d5f35f897fd5b610b831111a
Erik Vold <erikvvold@gmail.com>
2010-09-18 19:13:04 -0700

    lazy load nsIIOService

--- modules/constants.js
+++ modules/constants.js
@@ -8,7 +8,11 @@ var EXPORTED_SYMBOLS = [
 const Cc = Components.classes;
 const Ci = Components.interfaces;
 
+Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
+
+XPCOMUtils.defineLazyServiceGetter(
+    this, "ioService", "@mozilla.org/network/io-service;1",
+    "nsIIOService");
+
 const gmService = Cc["@scriptish.erikvold.com/scriptish-service;1"]
     .getService().wrappedJSObject;
-const ioService = Cc["@mozilla.org/network/io-service;1"]
-    .getService(Ci.nsIIOService);
