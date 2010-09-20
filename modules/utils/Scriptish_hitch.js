01d5f59808a96352866f1b58069973d9710f609e
Erik Vold <erikvvold@gmail.com>
2010-09-20 16:44:54 -0700

    some simplifications to Scriptish_hitch

--- modules/utils/Scriptish_hitch.js
+++ modules/utils/Scriptish_hitch.js
@@ -1,24 +1,20 @@
 
-// JSM exported symbols
 var EXPORTED_SYMBOLS = ["Scriptish_hitch"];
 
-const Scriptish_hitch = function (obj, meth) {
-  if (!obj[meth]) {
+const Scriptish_hitch = function (aObj, aMeth) {
+  if (!aObj[aMeth])
     throw "method '" + meth + "' does not exist on object '" + obj + "'";
-  }
 
-  var staticArgs = Array.prototype.splice.call(arguments, 2, arguments.length);
+  var staticArgs = Array.slice(arguments, 2);
 
   return function() {
-    // make a copy of staticArgs (don't modify it because it gets reused for
-    // every invocation).
-    var args = Array.prototype.slice.call(staticArgs);
-
-    // add all the new arguments
-    Array.prototype.push.apply(args, arguments);
+    // make a copy of staticArgs
+    var args = Array.slice(staticArgs);
+    // add all the new arguments.
+    args.push.apply(args, arguments);
 
     // invoke the original function with the correct this obj and the combined
     // list of static and dynamic arguments.
-    return obj[meth].apply(obj, args);
+    return aObj[aMeth].apply(aObj, args);
   };
 }
