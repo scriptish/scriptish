21c60c6c738b41e8de9e0a1607c2f20f3eaa44c7
Erik Vold <erikvvold@gmail.com>
2010-09-21 03:50:44 -0700

    updating build.sh to work for Scriptish

--- build.sh
+++ build.sh
@@ -2,60 +2,30 @@
 
 # Set up variables
 if [ "official" = "$1" ]; then
-    # For official builds, use the version in install.rdf.
-	GMVER=`sed -ne '/em:version/{ s/.*>\(.*\)<.*/\1/; p}' install.rdf`
+  # For official builds, use the version in install.rdf.
+  VER=`sed -ne '/em:version/{ s/.*>\(.*\)<.*/\1/; p}' install.rdf`
 else
-    # For beta builds, generate a version number.
-    GMVER=`date +"%Y.%m.%d.beta"`
+  # For beta builds, generate a version number.
+  VER=`date +"%Y.%m.%d.beta"`
 fi
-GMXPI="greasemonkey-$GMVER.xpi"
+XPI="scriptish-$VER.xpi"
 
 # Copy base structure to a temporary build directory and change to it
 echo "Creating working directory ..."
 rm -rf build
 mkdir build
 cp -r \
-	chrome.manifest components content defaults install.rdf locale skin \
-	    CREDITS LICENSE.bsd LICENSE.mit LICENSE.mpl \
-	build/
+  chrome.manifest components content defaults install.rdf license LICENSE.txt \
+       locale modules skin \
+  build/
 cd build
-
-if [ "official" != "$1" ]; then
-  echo "Patching install.rdf version ..."
-  sed -e "s/<em:version>.*<\/em:version>/<em:version>$GMVER<\/em:version>/" \
-    install.rdf > tmp
-  cat tmp > install.rdf
-  rm tmp
-fi
-
-echo "Gathering all locales into chrome.manifest ..."
-for entry in locale/*; do
-  entry=`basename $entry`
-  if [ $entry != en-US ]; then
-    echo "locale  greasemonkey  $entry  locale/$entry/" >> chrome.manifest
-  fi
-done
-
-echo "Creating greasemonkey.jar ..."
-sed \
-    -e "s/^content  *\([^ ]*\)  *\([^ ]*\)/content \1 jar:chrome\/greasemonkey.jar!\/\2/" \
-    -e "s/^skin  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)/skin \1 \2 jar:chrome\/greasemonkey.jar!\/\3/" \
-    -e "s/^locale  *\([^ ]*\)  *\([^ ]*\)  *\([^ ]*\)/locale \1 \2 jar:chrome\/greasemonkey.jar!\/\3/" \
-    chrome.manifest > tmp
-cat tmp > chrome.manifest
-rm tmp
-find content skin locale | sort | \
-  zip -r -0 -@ "greasemonkey.jar" > /dev/null
-rm -fr content/ skin/ locale/
-mkdir chrome
-mv greasemonkey.jar chrome/
 
 echo "Cleaning up unwanted files ..."
 find . -depth -name '*~' -exec rm -rf "{}" \;
 find . -depth -name '#*' -exec rm -rf "{}" \;
 
-echo "Creating $GMXPI ..."
-zip -qr9X "../$GMXPI" *
+echo "Creating $XPI ..."
+zip -qr9XD "../$XPI" *
 
 echo "Cleaning up temporary files ..."
 cd ..
