greasemonkey/greasemonkey

Define the updateDate for FF4 Addon objects.

content/script.js
@@ -41,6 +41,7 @@ Script.prototype = {
     GM_getConfig()._changed(this, event, data);
   },
 
+  get modifiedDate() { return new Date(parseInt(this._modified))  ; },
   get name() { return this._name; },
   get namespace() { return this._namespace; },
   get id() {

scriptish/scriptish

modules/script/script.js
@@ -89,6 +89,8 @@ Script.prototype = {
     return perms;
   },
 
+  get updateDate () { return new Date(parseInt(this._modified)); },
+
   findUpdates: function(aListener) {
     if ("onNoCompatibilityUpdateAvailable" in aListener)
       aListener.onNoCompatibilityUpdateAvailable(this);