817f46c88913d4469338928e4a27a463d0a94959
Erik Vold <erikvvold@gmail.com>
2010-09-25 22:36:59 -0700

    using NetUtil

--- modules/utils/Scriptish_stringBundle.js
+++ modules/utils/Scriptish_stringBundle.js
@@ -4,6 +4,7 @@ var EXPORTED_SYMBOLS = ["Scriptish_stringBundle"];
 const Cu = Components.utils;
 Cu.import("resource://scriptish/constants.js");
 Cu.import("resource://scriptish/prefmanager.js");
+Cu.import("resource://gre/modules/NetUtil.jsm");
 
 const chromeReg = Cc["@mozilla.org/chrome/chrome-registry;1"]
     .getService(Ci.nsIChromeRegistry);
@@ -13,7 +14,7 @@ const strBundleSrv = Cc["@mozilla.org/intl/stringbundle;1"]
 var defaultBundle = strBundleSrv.createBundle("chrome://scriptish/locale/scriptish.properties");
 var engBundle = strBundleSrv.createBundle((function(){
   var tmp = chromeReg.convertChromeURL(
-      ioService.newURI("chrome://scriptish/locale/scriptish.properties", "UTF-8", null))
+      NetUtil.newURI("chrome://scriptish/locale/scriptish.properties"))
       .spec.split("/");
   tmp[tmp.length - 2] = "en-US";
   return tmp.join("/")
