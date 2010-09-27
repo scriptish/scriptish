f60746c6a682299380af79ecaafd9d6bd0b3f9a1
Erik Vold <erikvvold@gmail.com>
2010-09-26 21:27:30 -0700

    using Services.jsm in modules/utils/Scriptish_stringBundle.js

--- modules/utils/Scriptish_stringBundle.js
+++ modules/utils/Scriptish_stringBundle.js
@@ -1,20 +1,16 @@
-
 var EXPORTED_SYMBOLS = ["Scriptish_stringBundle"];
 
 const Cu = Components.utils;
-Cu.import("resource://scriptish/constants.js");
 Cu.import("resource://scriptish/prefmanager.js");
 Cu.import("resource://gre/modules/NetUtil.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
 
-const chromeReg = Cc["@mozilla.org/chrome/chrome-registry;1"]
-    .getService(Ci.nsIChromeRegistry);
-const strBundleSrv = Cc["@mozilla.org/intl/stringbundle;1"]
-    .getService(Ci.nsIStringBundleService);
-
-var defaultBundle = strBundleSrv.createBundle("chrome://scriptish/locale/scriptish.properties");
-var engBundle = strBundleSrv.createBundle((function(){
-  var tmp = chromeReg.convertChromeURL(
-      NetUtil.newURI("chrome://scriptish/locale/scriptish.properties"))
+const defaultBundle = Services.strings.createBundle("chrome://scriptish/locale/scriptish.properties");
+const engBundle = Services.strings.createBundle((function(){
+  var tmp = Components.classes["@mozilla.org/chrome/chrome-registry;1"]
+      .getService(Components.interfaces.nsIChromeRegistry)
+      .convertChromeURL(
+          NetUtil.newURI("chrome://scriptish/locale/scriptish.properties"))
       .spec.split("/");
   tmp[tmp.length - 2] = "en-US";
   return tmp.join("/")
