6c5814ce22c944ae3b8beb2c7a45666bf5e40b1c
Erik Vold <erikvvold@gmail.com>
2010-10-02 14:26:21 -0700

    removing lazyGetter from last commit

--- modules/utils/Scriptish_openManager.js
+++ modules/utils/Scriptish_openManager.js
@@ -1,9 +1,6 @@
 var EXPORTED_SYMBOLS = ["Scriptish_openManager"];
-Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
 Components.utils.import("resource://gre/modules/Services.jsm");
-XPCOMUtils.defineLazyGetter(this, "Scriptish_openManager", function() {
-  return function Scriptish_openManager() {
-    Services.wm.getMostRecentWindow("navigator:browser")
-        .BrowserOpenAddonsMgr("addons://list/userscript");
-  }
-});
+function Scriptish_openManager() {
+  Services.wm.getMostRecentWindow("navigator:browser")
+      .BrowserOpenAddonsMgr("addons://list/userscript");
+}
