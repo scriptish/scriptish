0948ddddc71bc91c8aaf5a2a4ac91357fc7bdeab
Erik Vold <erikvvold@gmail.com>
2010-10-11 23:15:21 -0700

    minor style changes in content/js/updatecheck.js

--- extension/content/js/updatecheck.js
+++ extension/content/js/updatecheck.js
@@ -1,15 +1,15 @@
 // Checks if Scriptish was updated/installed
-(function() {
-  var tools = {};
-  Components.utils.import("resource://scriptish/prefmanager.js", tools);
-  Components.utils.import("resource://gre/modules/AddonManager.jsm", tools);
+(function(import, tools) {
+  import("resource://scriptish/prefmanager.js", tools);
+  import("resource://gre/modules/AddonManager.jsm", tools);
+  var pref = tools.Scriptish_prefRoot;
 
   // check if this is the first launch
-  if ("0.0" == tools.Scriptish_prefRoot.getValue("version", "0.0"))
-    tools.Scriptish_prefRoot.setValue("version", "0.0.1");
+  if ("0.0" == pref.getValue("version", "0.0"))
+    pref.setValue("version", "0.0.1");
 
   // update the currently initialized version so we don't do this work again.
   tools.AddonManager.getAddonByID("scriptish@erikvold.com", function(aAddon) {
-    tools.Scriptish_prefRoot.setValue("version", aAddon.version);
+    pref.setValue("version", aAddon.version);
   });
-})()
+})(Components.utils.import, {})
