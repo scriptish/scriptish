6a1d4fc2b6b6e4b76b8089c49301502439334d92
Erik Vold <erikvvold@gmail.com>
2010-10-12 01:14:03 -0700

    minor style change to modules/utils/Scriptish_config.js

--- extension/modules/utils/Scriptish_config.js
+++ extension/modules/utils/Scriptish_config.js
@@ -1,6 +1,4 @@
 var EXPORTED_SYMBOLS = ["Scriptish_config"];
 Components.utils.import("resource://scriptish/constants.js");
-Components.utils.import("resource://gre/modules/XPCOMUtils.jsm");
-XPCOMUtils.defineLazyGetter(this, "Scriptish_config", function() {
-  return Services.scriptish.config;
-});
+XPCOMUtils.defineLazyGetter(
+    this, "Scriptish_config", function() Services.scriptish.config);
