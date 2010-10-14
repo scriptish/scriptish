75c497b8de7c769aa999e8cd933056f02e5e7142
sizzlemctwizzle <medleymind@gmail.com>
2010-10-14 15:11:07 -0500

    Add the toolbar button to the add-on bar by default.

--- extension/content/js/updatecheck.js
+++ extension/content/js/updatecheck.js
@@ -3,13 +3,26 @@
   import("resource://scriptish/prefmanager.js", tools);
   import("resource://scriptish/constants.js", tools);
   var pref = tools.Scriptish_prefRoot;
-
-  // check if this is the first launch
-  if ("0.0" == pref.getValue("version", "0.0"))
-    pref.setValue("version", "0.0.1");
+  var currentVer = pref.getValue("version", "0.0");
 
   // update the currently initialized version so we don't do this work again.
   tools.AddonManager.getAddonByID("scriptish@erikvold.com", function(aAddon) {
     pref.setValue("version", aAddon.version);
   });
+
+  if (0 >= tools.Services.vc.compare(currentVer, "0.1b5")) {
+    // add toolbaritem to add-on bar
+    var chromeWin = tools.Services.wm.getMostRecentWindow("navigator:browser");
+    var gNavToolbox = chromeWin.gNavToolbox;
+    var gAddonBar = chromeWin.document.getElementById("addon-bar");
+    var addonSet = gAddonBar.getAttribute("currentset").split(",");
+    var curSet = gNavToolbox.getAttribute("currentset").split(",");
+    curSet = curSet.concat(addonSet);
+    if (0 <= curSet.indexOf("scriptish-tb-item")) return;
+    addonSet.push("scriptish-tb-item");
+    addonSet = addonSet.join(",");
+    gAddonBar.setAttribute("currentset", addonSet);
+    gAddonBar.currentSet = addonSet;
+    chromeWin.BrowserToolboxCustomizeDone(true);
+  }
 })(Components.utils.import, {})
