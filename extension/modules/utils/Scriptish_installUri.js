78291bfa1ebd03b5db445b205152f671136c005c
Erik Vold <erikvvold@gmail.com>
2010-11-01 13:10:31 -0700

    using faster timeout for Scriptish_installUri

--- extension/modules/utils/Scriptish_installUri.js
+++ extension/modules/utils/Scriptish_installUri.js
@@ -1,11 +1,10 @@
 var EXPORTED_SYMBOLS = ["Scriptish_installUri"];
-Components.utils.import("resource://scriptish/third-party/Timer.js");
+Components.utils.import("resource://scriptish/constants.js");
 Components.utils.import("resource://scriptish/config/configdownloader.js");
 
-var gTimer = new Timer();
 function Scriptish_installUri(aURI, aWin) {
   // docs for nsicontentpolicy say we're not supposed to block, so short timer.
-  gTimer.setTimeout(function() {
+  timeout(function() {
     Scriptish_configDownloader.startInstall(aURI, aWin);
   }, 0);
 }
