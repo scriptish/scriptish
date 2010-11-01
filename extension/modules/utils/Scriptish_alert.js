fdaca1b8582d56df1a80ac5942214bcd442221b7
Erik Vold <erikvvold@gmail.com>
2010-11-01 12:50:26 -0700

    using faster timeout in Scriptish_alert

--- extension/modules/utils/Scriptish_alert.js
+++ extension/modules/utils/Scriptish_alert.js
@@ -1,9 +1,8 @@
 var EXPORTED_SYMBOLS = ["Scriptish_alert"];
 Components.utils.import("resource://scriptish/constants.js");
-Components.utils.import("resource://scriptish/third-party/Timer.js");
-const gTimer = new Timer();
+
 function Scriptish_alert(aMsg, aWait) {
   if (typeof aWait == "number")
-    return gTimer.setTimeout(function() Scriptish_alert(aMsg), aWait);
+    return timeout(function() Scriptish_alert(aMsg), aWait);
   Services.prompt.alert(null, "Scriptish", aMsg+"");
 }
