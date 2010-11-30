7f16385736cf0b5f3c1d32e7ab7f94605f4cd273
Erik Vold <erikvvold@gmail.com>
2010-11-29 21:07:27 -0800

    some minor simplifications to Scriptish_cryptoHash()

--- extension/modules/utils/Scriptish_cryptoHash.js
+++ extension/modules/utils/Scriptish_cryptoHash.js
@@ -2,53 +2,22 @@ var EXPORTED_SYMBOLS = ["Scriptish_cryptoHash"];
 Components.utils.import("resource://scriptish/constants.js");
 
 function Scriptish_cryptoHash(aString, aAlg, aCharset) {
+  let ch = Instances.ch;
+  let unicodeConverter = Instances.suc;
+  let str = aString + "";
+  let alg = ((aAlg || "SHA1") + "").trim().toUpperCase();
+  let charset = (aCharset || "UTF-8") + "";
+
   try {
-    aString = aString.toString();
+    ch.initWithString(alg);
+  } catch (e) {
+    throw new Error("Invalid hash algorithm specified.");
+  }
+
+  try {
+    unicodeConverter.charset = charset;
   } catch(e) {
-    throw new Error("Unable to convert argument into a string.");
-  }
-
-  var ch = Instances.ch;
-
-  if (aAlg) {
-    try {
-      aAlg = aAlg.trim().toUpperCase();
-    } catch(e) {}
-  } else {
-    aAlg = "SHA1";
-  }
-  switch (aAlg) {
-    case "MD2":
-      aAlg = ch.MD2;
-      break;
-    case "MD5":
-      aAlg = ch.MD5;
-      break;
-    case "SHA1":
-      aAlg = ch.SHA1;
-      break;
-    case "SHA256":
-      aAlg = ch.SHA256;
-      break;
-    case "SHA384":
-      aAlg = ch.SHA384;
-      break;
-    case "SHA512":
-      aAlg = ch.SHA512;
-      break;
-    default:
-      throw new Error("Invalid hash algorithm specified.");
-  }
-
-  var unicodeConverter = Instances.suc;
-  unicodeConverter.charset = "UTF-8";
-
-  if (aCharset) {
-    try {
-      unicodeConverter.charset = aCharset.trim().toUpperCase();
-    } catch(e) {
-      throw new Error("Invalid charset specified.");
-    }
+    throw new Error("Invalid charset specified.");
   }
 
   // Make sure we're working with Unicode
@@ -57,7 +26,6 @@ function Scriptish_cryptoHash(aString, aAlg, aCharset) {
 
   var data = unicodeConverter.convertToByteArray(aString, {});
 
-  ch.init(aAlg);
   ch.update(data, data.length);
   var hash = ch.finish(false); // hash as raw octets
 
