018102f2789eb3b5dbf40f1b393a36d000f7767b
Erik Vold <erikvvold@gmail.com>
2010-11-30 21:40:13 -0800

    telling updateFromStream to read the entire string

--- extension/modules/utils/Scriptish_cryptoHash.js
+++ extension/modules/utils/Scriptish_cryptoHash.js
@@ -1,6 +1,9 @@
 var EXPORTED_SYMBOLS = ["Scriptish_cryptoHash"];
 Components.utils.import("resource://scriptish/constants.js");
 
+// this tells updateFromStream to read the entire string 
+const PR_UINT32_MAX = 0xffffffff;
+
 function Scriptish_cryptoHash(aString, aAlg, aCharset) {
   let ch = Instances.ch;
   let unicodeConverter = Instances.suc;
@@ -20,7 +23,7 @@ function Scriptish_cryptoHash(aString, aAlg, aCharset) {
     throw new Error("Invalid charset specified.");
   }
 
-  ch.updateFromStream(unicodeConverter.convertToInputStream(str), str.length);
+  ch.updateFromStream(unicodeConverter.convertToInputStream(str), PR_UINT32_MAX);
   let hash = ch.finish(false); // hash as raw octets
   return [("0" + hash.charCodeAt(i).toString(16)).slice(-2) for (i in hash)]
       .join("");
