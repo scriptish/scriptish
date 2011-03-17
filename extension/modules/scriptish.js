3f063571f96daae08e1042fd07657e566405c2a8
Erik Vold <erikvvold@gmail.com>
2011-03-16 21:50:36 -0700

    removing the separate file for Scriptish_isGreasemonkeyable

--- extension/modules/scriptish.js
+++ extension/modules/scriptish.js
@@ -1,10 +1,34 @@
 var EXPORTED_SYMBOLS = ["Scriptish"];
 Components.utils.import("resource://scriptish/constants.js");
 Components.utils.import("resource://scriptish/prefmanager.js");
-Components.utils.import("resource://scriptish/utils/Scriptish_isGreasemonkeyable.js");
 const Scriptish = {
   get config() Services.scriptish.config,
   get enabled() Scriptish_prefRoot.getValue("enabled", true),
   set enabled(aVal) Scriptish_prefRoot.setValue("enabled", !!aVal),
-  isGreasemonkeyable: Scriptish_isGreasemonkeyable
+  isGreasemonkeyable: function Scriptish_isGreasemonkeyable(aURL) {
+    // if the url provide is not a valid url, then an error could be thrown
+    try {
+      var scheme = Services.io.extractScheme(aURL);
+    } catch (e) {
+      return false;
+    }
+
+    switch (scheme) {
+      case "http":
+      case "https":
+      case "ftp":
+      case "data":
+        return true;
+      case "about":
+        // Always allow "about:blank".
+        if (/^about:blank/.test(aURL)) return true;
+        // Conditionally allow the rest of "about:".
+        return Scriptish_prefRoot.getValue('aboutIsGreaseable');
+      case "file":
+        return Scriptish_prefRoot.getValue('fileIsGreaseable');
+      case "unmht":
+        return Scriptish_prefRoot.getValue('unmhtIsGreaseable');
+    }
+    return false;
+  }
 }
