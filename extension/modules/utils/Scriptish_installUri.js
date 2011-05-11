46d4c70bee6310301b8429d0e3cd0a9f8fce66ed
Erik Vold <erikvvold@gmail.com>
2011-05-11 14:54:06 -0700

    aWin argument for Scriptish_installUri is now optional (for Greasefire or w/e other addon that wants to use it)

--- extension/modules/utils/Scriptish_installUri.js
+++ extension/modules/utils/Scriptish_installUri.js
@@ -3,6 +3,8 @@ Components.utils.import("resource://scriptish/constants.js");
 Components.utils.import("resource://scriptish/config/configdownloader.js");
 
 function Scriptish_installUri(aURI, aWin) {
+  if (!aWin) aWin = Services.wm.getMostRecentWindow("navigator:browser");
+
   // docs for nsicontentpolicy say we're not supposed to block, so short timer.
   timeout(function() Scriptish_configDownloader.startInstall(aURI, aWin));
 }
