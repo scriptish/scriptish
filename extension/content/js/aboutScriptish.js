06858b493906ad8c635f1e72a4545290bc1303cf
supahgreg <greg.parris@gmail.com>
2011-05-24 10:15:19 -0500

    Generate the 'about:scriptish' contributor/translator lists from install.rdf.

--- extension/content/js/aboutScriptish.js
+++ extension/content/js/aboutScriptish.js
@@ -1,5 +1,6 @@
 $(function() {
   if (window.location.href.split("?")[1] == "test") {
+    // Run tests
     var include = function(aSrc, aCallback) {
       var script = document.createElement("script");
       script.src = aSrc;
@@ -14,5 +15,28 @@ $(function() {
         runTests();
       });
     });
+    return;
   }
+
+  // Show about:scriptish
+  var tools = {};
+  Components.utils.import("resource://gre/modules/AddonManager.jsm", tools);
+
+  var addPerson = function(aTarget, aPerson) {
+    var person = aPerson.name.split(/; +/);
+    if (person[1]) {
+      person = person[0] + " &mdash; "
+          + "<a href='" + person[1] + "'>"
+          + person[1].replace(/^mailto:/i, "") + "</a>";
+    }
+    aTarget.append("<li>" + person + "</li>");
+  }
+
+  tools.AddonManager.getAddonByID("scriptish@erikvold.com", function(aAddon) {
+    var $contlist = $("#contlist");
+    var $translist = $("#translist");
+
+    $.each(aAddon.contributors, function(i, val) addPerson($contlist, val));
+    $.each(aAddon.translators, function(i, val) addPerson($translist, val));
+  });
 });
