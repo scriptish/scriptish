1f8f23c13e2e8a759b78334e1f6827d2d0a1dc92
Erik Vold <erikvvold@gmail.com>
2011-05-24 16:57:00 -0700

    no longer using jquery on about:scriptish, and only using it for about:scriptish?test

--- extension/content/js/aboutScriptish.js
+++ extension/content/js/aboutScriptish.js
@@ -1,18 +1,23 @@
-$(function() {
+const {classes: Cc, interfaces: Ci, utils: Cu} = Components;
+
+(function() {
   if (window.location.href.split("?")[1] == "test") {
+    var head = document.documentElement.firstChild;
     var include = function(aSrc, aCallback) {
       var script = document.createElement("script");
       script.src = aSrc;
       script.addEventListener("load", aCallback, false);
-      $("head")[0].appendChild(script);
+      head.appendChild(script);
     }
 
-    include("test/qunit.js", function() {
-      include("test/runTests.js", function() {
-        $("#main").hide();
-        $("#test").show();
-        runTests();
+    include("test/jquery-1.6.1.min.js", function() {
+      include("test/qunit.js", function() {
+        include("test/runTests.js", function() {
+          $("#main").hide();
+          $("#test").show();
+          runTests();
+        });
       });
     });
   }
-});
+})();
