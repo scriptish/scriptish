667c94ae0163a3d7810024915a6dea11f4939da8
Erik Vold <erikvvold@gmail.com>
2011-05-24 00:33:30 -0700

    Closes #310 removing checkmark in page title for about:scriptish

--- extension/content/js/aboutScriptish.js
+++ extension/content/js/aboutScriptish.js
@@ -1,7 +1,18 @@
 $(function() {
-  var doTest = window.location.href.split("?")[1] == "test";
-  if (!doTest) return;
-  $("#main").hide();
-  $("#test").show();
-  runTests();
+  if (window.location.href.split("?")[1] == "test") {
+    var include = function(aSrc, aCallback) {
+      var script = document.createElement("script");
+      script.src = aSrc;
+      script.addEventListener("load", aCallback, false);
+      $("head")[0].appendChild(script);
+    }
+
+    include("test/qunit.js", function() {
+      include("test/runTests.js", function() {
+        $("#main").hide();
+        $("#test").show();
+        runTests();
+      });
+    });
+  }
 });
