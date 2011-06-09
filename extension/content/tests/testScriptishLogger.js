cf77e74d75757305239c0bd04e2d21aba499fd93
Erik Vold <erikvvold@gmail.com>
2011-06-08 21:56:45 -0700

    test: Scriptish_log removes null chars from messages

--- extension/content/tests/testScriptishLogger.js
+++ extension/content/tests/testScriptishLogger.js
@@ -29,18 +29,18 @@ asyncTest("extensions.scriptish.logChrome", 2, function() {
     logChrome = PREF_BRANCH.getBoolPref("logChrome");
   } catch (e) {}
 
+  tID = setTimeout(function() {
+    unregListener();
+    ok(!logChrome, "message was not logged");
+    start();
+  }, 500);
+
   try {
     Scriptish_log("TEST");
     ok(true, "sent message without error being thrown");
   } catch (e) {
     ok(false, e);
   }
-
-  tID = setTimeout(function() {
-    unregListener();
-    ok(!logChrome, "message was not logged");
-    start();
-  }, 500);
 });
 
 
@@ -63,16 +63,52 @@ asyncTest("forced log message", 2, function() {
   Services.cs.registerListener(consoleListener);
   function unregListener() Services.cs.unregisterListener(consoleListener);
 
+  tID = setTimeout(function() {
+    unregListener();
+    ok(false, "message was not logged");
+    start();
+  }, 500);
+
   try {
     Scriptish_log("FORCED TEST", true);
     ok(true, "sent message without error being thrown");
   } catch (e) {
     ok(false, e);
   }
+});
+
+asyncTest("null char removed", 2, function() {
+  var tID, msg = "FORCED \0TEST \0WITH \0NULLS";
+
+  // making console listener
+  var consoleListener = {
+    observe: function({message}) {
+      if ("[Scriptish] FORCED TEST WITH NULLS" == message || msg == message) {
+        if ("[Scriptish] FORCED TEST WITH NULLS" == message)
+          ok(true, "message was logged successfully as expected");
+        else if (msg == message)
+          ok(false, "null chars were not removed");
+        clearTimeout(tID);
+        unregListener();
+        start();
+      }
+    }
+  };
+
+  // add console listener
+  Services.cs.registerListener(consoleListener);
+  function unregListener() Services.cs.unregisterListener(consoleListener);
 
   tID = setTimeout(function() {
     unregListener();
     ok(false, "message was not logged");
     start();
   }, 500);
+
+  try {
+    Scriptish_log(msg, true);
+    ok(true, "sent message without error being thrown");
+  } catch (e) {
+    ok(false, e);
+  }
 });
