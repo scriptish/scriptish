2f496a58cea415cf6ee6078c794dcf5bc95d836d
Erik Vold <erikvvold@gmail.com>
2011-06-09 10:35:33 -0700

    Closes #356 only run qunit tests if the first url param of about:scriptish is 'test' or 'filter'

--- extension/content/js/aboutScriptish.js
+++ extension/content/js/aboutScriptish.js
@@ -20,7 +20,7 @@ Cu.import("resource://scriptish/constants.js");
 
   // Show about:scriptish?test
   var params = window.location.href.split("?")[1];
-  if (/(?:^|&)test(?:&|=|$)/i.test(params)) {
+  if (/(?:^|&)(?:test|filter)(?:&|=|$)/i.test(params)) {
     Cu.import("resource://scriptish/utils/q.js");
     include("tests/runTests.js").then(function() {
       $("main").style.display = "none";
