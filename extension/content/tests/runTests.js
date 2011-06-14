a0bd72f6b2222f4549eef5b02603eda28c31212e
Erik Vold <erikvvold@gmail.com>
2011-06-14 08:56:29 -0700

    test: Scriptish_convert2RegExp

--- extension/content/tests/runTests.js
+++ extension/content/tests/runTests.js
@@ -8,6 +8,7 @@ function runTests() {
     function() include("tests/testGetConfig.js"),
     function() include("tests/testScriptishEnabled.js"),
     function() include("tests/testScriptishUpdateSecurely.js"),
+    function() include("tests/testScriptishConvert2RegExp.js"),
     function() include("tests/testScriptishLogger.js"),
     function() include("tests/testTimeout.js"),
     function() include("tests/endTests.js")).then(function() {
