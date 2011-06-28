081b88216676ef02b8dc9d8c623bf2106fee851f
Nils Maier <maierman@web.de>
2011-06-28 18:30:13 +0200

    Scratchpad integration: Make the code comprehensible
    Also allow other extensions to check/use `Scratchpad.saveAsUserscript`

--- extension/content/js/scratchpad.js
+++ extension/content/js/scratchpad.js
@@ -1,29 +1,25 @@
-(function(inc, tools) {
+(function(inc, _) {
+  "use strict";
   inc("resource://scriptish/utils/Scriptish_stringBundle.js", tools);
 
   function $(aID) document.getElementById(aID);
 
+  this.saveAsUserscript = function() {
+    let content = $('scratchpad-textbox').value;
+    this.browserWindow.Scriptish_BrowserUIM.newUserScript(content);
+  };
+
   window.addEventListener("DOMContentLoaded", function init() {
     window.removeEventListener("DOMContentLoaded", init, false);
-    // command
-    $("sp-cmd-scriptish-savetouserscript").setAttribute("oncommand",
-        "Scratchpad.browserWindow.Scriptish_BrowserUIM.newUserScript(document.getElementById('scratchpad-textbox').value);");
-
     // key
-    var tmp = $("sp-key-scriptish-savetouserscript")
+    var tmp = $("sp-key-scriptish-saveasuserscript")
     tmp.setAttribute("key",
         tools.Scriptish_stringBundle("scratchpad.saveAsUserScript.ak"));
     tmp.setAttribute("modifiers", "accel");
-    tmp.setAttribute("command", "sp-cmd-scriptish-savetouserscript");
 
     // menuitem
-    tmp = $("sp-menu-scriptish-savetouserscript");
+    tmp = $("sp-cmd-scriptish-saveasuserscript");
     tmp.setAttribute("label",
         tools.Scriptish_stringBundle("scratchpad.saveAsUserScript"));
-    tmp.setAttribute("accesskey",
-        tools.Scriptish_stringBundle("scratchpad.saveAsUserScript.ak"));
-    tmp.setAttribute("key", "sp-key-scriptish-savetouserscript");
-    tmp.setAttribute("command", "sp-cmd-scriptish-savetouserscript");
-    $("sp-menu-filepopup").insertBefore(tmp, $("sp-menu-saveas").nextSibling);
   }, false);
-})(Components.utils.import, {});
+}).call(Scratchpad, Components.utils.import, {});
