fac6bb4fd98fb7923d573d13a950736e58b2ec96
Nils Maier <maierman@web.de>
2011-06-28 14:28:23 +0200

    Put the XUL/DOM localization stuff into a module for reuse

--- extension/content/js/options.js
+++ extension/content/js/options.js
@@ -2,6 +2,7 @@
 
 Components.utils.import("resource://scriptish/scriptish.js");
 Components.utils.import("resource://scriptish/logging.js");
+Components.utils.import("resource://scriptish/utils/Scriptish_localizeDOM.js");
 Components.utils.import("resource://scriptish/utils/Scriptish_stringBundle.js");
 
 function $(aID) document.getElementById(aID);
@@ -18,36 +19,7 @@ function saveExcludes() {
   Scriptish.notify(null, "scriptish-preferences-change", true);
 }
 
-/*
- * Localize XUL elements (as defined by localize-attributes) from a stringbundle
- * @author Nils Maier
- */
-addEventListener("DOMContentLoaded", function localize() {
-  removeEventListener("DOMContentLoaded", localize, false);
-
-  let nodes = document.querySelectorAll("*[localize]");
-  for (let i = 0, e = nodes.length, n; i < e; ++i) {
-    n = nodes[i];
-    let localized = n.getAttribute("localize").split(",");
-    for each (let l in localized) {
-      try {
-        let s = Scriptish_stringBundle(n.getAttribute(l));
-        n.setAttribute(l, s);
-
-        // also localize panes
-        if (/^pane-/.test(n.id)) {
-          n = document.documentElement._selector.querySelector("*[pane=\"" + n.id + "\"]");
-          if (n) {
-            n.setAttribute(l, s);
-          }
-        }
-      }
-      catch (ex) {
-        Scriptish_log("Failed to set localized attribute; l=" + l);
-      }
-    }
-  }
-}, false);
+Scriptish_localizeOnLoad(this);
 
 addEventListener("load", function init() {
   removeEventListener("load", init, false);
