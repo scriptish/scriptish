b102f271a6f15f650f29bb7cb971fb0530c7e789
Nils Maier <maierman@web.de>
2011-07-03 22:52:55 +0200

    Clean up import list and hiddenDOMWindow use: scriptresource.js

--- extension/modules/script/scriptresource.js
+++ extension/modules/script/scriptresource.js
@@ -1,7 +1,7 @@
 var EXPORTED_SYMBOLS = ["ScriptResource"];
 Components.utils.import("resource://scriptish/constants.js");
-Components.utils.import("resource://scriptish/utils/Scriptish_getBinaryContents.js");
 Components.utils.import("resource://scriptish/script/scriptdependency.js");
+lazyImport(this, "resource://scriptish/utils/Scriptish_getBinaryContents.js", ["Scriptish_getBinaryContents"]);
 
 function ScriptResource() {
   ScriptDependency.apply(this, arguments);
@@ -11,7 +11,6 @@ ScriptResource.prototype = new ScriptDependency();
 ScriptResource.prototype.constructor = ScriptResource;
 ScriptResource.prototype.__defineGetter__("name", function() this._name);
 ScriptResource.prototype.__defineGetter__("dataContent", function() {
-  var win = Services.ass.hiddenDOMWindow;
   var binaryContents = Scriptish_getBinaryContents(this._file);
   var mimetype = this._mimetype;
 
@@ -19,5 +18,5 @@ ScriptResource.prototype.__defineGetter__("dataContent", function() {
     mimetype += ";charset=" + this._charset;
 
   return "data:" + mimetype + ";base64," +
-      win.encodeURIComponent(win.btoa(binaryContents));
+      encodeURIComponent(btoa(binaryContents));
 });
