74733d89b50735121bdb6eb5f9088f6cef052147
Erik Vold <erikvvold@gmail.com>
2011-07-05 10:03:20 -0700

    Rewrote Script.header_parse so that a whole script's contents are no longer parsed, just the top section (assuming there is a UserScript header).
    This is a old tip from @satyr which I hadn't gotten around to, thanks!

--- extension/content/tests/runTests.js
+++ extension/content/tests/runTests.js
@@ -66,6 +66,7 @@ function runTests() {
     function() include("tests/testLazyImport.js"),
     function() include("tests/testCachedResource.js"),
     function() include("tests/testCryptoHash.js"),
+    function() include("tests/testScriptHeaderParse.js"),
     function() include("tests/testToolsMenuItem.js"),
     function() include("tests/testGetConfig.js"),
     function() include("tests/testIsGMable.js"),
---
+++ extension/content/tests/testScriptHeaderParse.js
@@ -0,0 +1,97 @@
+module("Script.header_parse");
+
+test("normal @keys (with space b4 @)", function() {
+  var parser = importModule("resource://scriptish/script/script.js").Script.header_parse;
+  var header = (<><![CDATA[
+  // ==UserScript==
+  // @id test-id
+  // @name test-name
+  // @version test-version
+  // @version test-version-II
+  // ==/UserScript==
+  ]]></>).toString();
+  var parsed = parser(header);
+
+  deepEqual(parsed.id, ["test-id"], "@id found");
+  deepEqual(parsed.name, ["test-name"], "@name found");
+  deepEqual(parsed.version, ["test-version", "test-version-II"], "@versions found");
+  equal(parsed.fail, undefined, "@fail not found");
+});
+
+test("normal @keys and following @fail", function() {
+  var parser = importModule("resource://scriptish/script/script.js").Script.header_parse;
+  var header = (<><![CDATA[
+  // ==UserScript==
+  // @id test-id
+  // @name test-name
+  // @version test-version
+  // @version test-version-II
+  // ==/UserScript==
+  // @fail test-fail
+  ]]></>).toString();
+  var parsed = parser(header);
+
+  deepEqual(parsed.id, ["test-id"], "@id found");
+  deepEqual(parsed.name, ["test-name"], "@name found");
+  deepEqual(parsed.version, ["test-version", "test-version-II"], "@versions found");
+  equal(parsed.fail, undefined, "@fail not found");
+});
+
+test("normal @keys and leading @fail", function() {
+  var parser = importModule("resource://scriptish/script/script.js").Script.header_parse;
+  var header = (<><![CDATA[
+  // @fail test-fail
+  // ==UserScript==
+  // @id test-id
+  // @name test-name
+  // @version test-version
+  // @version test-version-II
+  // ==/UserScript==
+  ]]></>).toString();
+  var parsed = parser(header);
+
+  deepEqual(parsed.id, ["test-id"], "@id found");
+  deepEqual(parsed.name, ["test-name"], "@name found");
+  deepEqual(parsed.version, ["test-version", "test-version-II"], "@versions found");
+  equal(parsed.fail, undefined, "@fail not found");
+});
+
+test("two sets of normal @keys", function() {
+  var parser = importModule("resource://scriptish/script/script.js").Script.header_parse;
+  var header = (<><![CDATA[
+  // ==UserScript==
+  // @id test-id
+  // @name test-name
+  // @version test-version
+  // @version test-version-II
+  // ==/UserScript==
+  // ==UserScript==
+  // @fail test-fail
+  // ==/UserScript==
+  ]]></>).toString();
+  var parsed = parser(header);
+
+  deepEqual(parsed.id, ["test-id"], "@id found");
+  deepEqual(parsed.name, ["test-name"], "@name found");
+  deepEqual(parsed.version, ["test-version", "test-version-II"], "@versions found");
+  equal(parsed.fail, undefined, "@fail not found");
+});
+
+test("@keys without space b4 @", function() {
+  var parser = importModule("resource://scriptish/script/script.js").Script.header_parse;
+  var header = (<><![CDATA[
+  //==UserScript==
+  //@id test-id
+  //@name test-name
+  //@version test-version
+  //@version test-version-II
+  //==/UserScript==
+  //@fail test-fail
+  ]]></>).toString();
+  var parsed = parser(header);
+
+  deepEqual(parsed.id, ["test-id"], "@id found");
+  deepEqual(parsed.name, ["test-name"], "@name found");
+  deepEqual(parsed.version, ["test-version", "test-version-II"], "@versions found");
+  equal(parsed.fail, undefined, "@fail not found");
+});
