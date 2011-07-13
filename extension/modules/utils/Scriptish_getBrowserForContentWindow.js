67b473ec4ad24dedc55110fa180a0c681e9b2cd5
supahgreg <greg.parris@gmail.com>
2011-07-13 09:31:50 -0500

    Use a different technique to get the browser window for a given content window.

--- extension/modules/utils/Scriptish_getBrowserForContentWindow.js
+++ extension/modules/utils/Scriptish_getBrowserForContentWindow.js
@@ -1,18 +1,13 @@
 var EXPORTED_SYMBOLS = ["Scriptish_getBrowserForContentWindow"];
 Components.utils.import("resource://scriptish/constants.js");
 
-function Scriptish_getBrowserForContentWindow(aWin) {
-  // enumerate through browser windows
-  let browserEnumerator = Services.wm.getEnumerator("navigator:browser");
-  while (browserEnumerator.hasMoreElements()) {
-    let browserWin = browserEnumerator.getNext();
-    let tabBrowser = browserWin.getBrowser();
-    if (!tabBrowser) continue;
-    for (let [, tab] in Iterator(tabBrowser.tabs)) {
-      if (!tab) continue;
-      let win = tab.linkedBrowser.contentWindow;
-      if (aWin === win || aWin.top === win) return browserWin;
-    }
-  }
-  return null;
+// See _getBrowserWindowForContentWindow in browser.js
+function Scriptish_getBrowserForContentWindow(aContentWindow) {
+  return aContentWindow.QueryInterface(Ci.nsIInterfaceRequestor)
+      .getInterface(Ci.nsIWebNavigation)
+      .QueryInterface(Ci.nsIDocShellTreeItem)
+      .rootTreeItem
+      .QueryInterface(Ci.nsIInterfaceRequestor)
+      .getInterface(Ci.nsIDOMWindow)
+      .wrappedJSObject;
 }
