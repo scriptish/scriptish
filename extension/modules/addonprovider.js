eea3a696522a74722f6bc84effd9f3aa3395b429
supahgreg <greg.parris@gmail.com>
2011-08-18 09:59:09 -0500

    using Scriptish_config in modules/addonprovider.js

--- extension/modules/addonprovider.js
+++ extension/modules/addonprovider.js
@@ -1,14 +1,16 @@
 var EXPORTED_SYMBOLS = [];
 Components.utils.import("resource://scriptish/constants.js");
 
+lazyImport(this, "resource://scriptish/config.js", ["Scriptish_config"]);
 lazyImport(this, "resource://scriptish/scriptish.js", ["Scriptish"]);
+
 lazyUtil(this, "popupNotification");
 lazyUtil(this, "stringBundle");
 
 const Scriptish_ScriptProvider = {
-  observe: function(aSubject, aTopic, aData) Scriptish.getConfig(function(config) {
+  observe: function(aSubject, aTopic, aData) {
     aData = JSON.parse(aData);
-    let script = config.getScriptById(aData.id);
+    let script = Scriptish_config.getScriptById(aData.id);
     switch(aTopic){
     case "scriptish-script-installed":
       AddonManagerPrivate.callInstallListeners(
@@ -79,7 +81,7 @@ const Scriptish_ScriptProvider = {
       AddonManagerPrivate.callAddonListeners("onUninstalled", script);
       break;
     }
-  }),
+  },
   QueryInterface: XPCOMUtils.generateQI([Ci.nsISupports, Ci.nsIObserver])
 };
 
@@ -95,17 +97,13 @@ if (AddonManagerPrivate.AddonType) {
 
 AddonManagerPrivate.registerProvider({
   getAddonByID: function(aId, aCallback) {
-    Scriptish.getConfig(function(config) {
-      aCallback(config.getScriptById(aId));
-    });
+    aCallback(Scriptish_config.getScriptById(aId));
   },
   getAddonsByTypes: function(aTypes, aCallback) {
     if (aTypes && aTypes.indexOf("userscript") < 0) {
       aCallback([]);
     } else {
-      Scriptish.getConfig(function(config) {
-        aCallback(config.scripts);
-      });
+      aCallback(Scriptish_config.scripts);
     }
   },
 
@@ -113,15 +111,13 @@ AddonManagerPrivate.registerProvider({
     if (aTypes && aTypes.indexOf("userscript") < 0) {
       aCallback([]);
     } else {
-      Scriptish.getConfig(function(config) {
-        let updates = [];
-        config.scripts.forEach(function(script) {
-          if (script.updateAvailable
-              && script.updateAvailable.state == AddonManager.STATE_AVAILABLE)
-            updates.push(script.updateAvailable);
-        });
-        aCallback(updates);
+      let updates = [];
+      Scriptish_config.scripts.forEach(function(script) {
+        if (script.updateAvailable
+            && script.updateAvailable.state == AddonManager.STATE_AVAILABLE)
+          updates.push(script.updateAvailable);
       });
+      aCallback(updates);
     }
   }
 }, types);
