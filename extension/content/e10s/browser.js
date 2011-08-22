dfbb709221e7bb5cb97e98cd8014bf9f753b5766
Erik Vold <erikvvold@gmail.com>
2011-08-22 09:17:04 -0700

    minor style changes in browser-contents.js

--- extension/content/e10s/browser.js
+++ extension/content/e10s/browser.js
@@ -8,15 +8,18 @@ lazyImport(global, "resource://scriptish/manager.js", ["Scriptish_manager"]);
 
 Scriptish_log("step 1a");
 
-// Check if Scriptish has been updated/installed
-Services.scriptish.updateChk && setTimeout(function() {
-  Services.scriptish.updateChk();
-}, 750);
+Scriptish_manager.setup(content);
+
+messageManager.loadFrameScript("chrome://scriptish/content/e10s/browser-content.js", true);
+
+addEventListener("load", function() {
+  // Check if Scriptish has been updated/installed
+  Services.scriptish.updateChk && setTimeout(function() {
+    Services.scriptish.updateChk();
+  }, 750);
+}, false);
+
 
 Scriptish_log("step 1b");
 
 })(Components.utils.import, {}, this);
-
-
-
-messageManager.loadFrameScript("chrome://scriptish/content/e10s/browser-content.js", true);
