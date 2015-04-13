scriptish/scriptish

commit 3fcb12c28f3b89a1ec17da3675f8a7ec41221604

Fix GM_log (because XRay wrappers literally hate me!)
Closes GH-258

extension/modules/utils/Scriptish_injectScripts.js
@@ -7,7 +7,7 @@ Cu.import("resource://scriptish/api/GM_sandboxScripts.js");
 lazyImport(this, "resource://scriptish/logging.js", ["Scriptish_log", "Scriptish_logError"]);
 lazyImport(this, "resource://scriptish/prefmanager.js", ["Scriptish_prefRoot"]);
 lazyImport(this, "resource://scriptish/api.js", ["GM_API"]);
-lazyImport(this, "resource://scriptish/api/GM_console.js", ["GM_console"]);
+lazyImport(this, "resource://scriptish/api/GM_console.js", ["GM_console", "GM_getConsoleFor"]);
 lazyImport(this, "resource://scriptish/api/GM_ScriptLogger.js", ["GM_ScriptLogger"]);
 
 lazyImport(this, "resource://scriptish/third-party/Scriptish_getBrowserForContentWindow.js", ["Scriptish_getBrowserForContentWindow"]);
@@ -93,15 +93,16 @@ function Scriptish_injectScripts(options) {
 
     if (!useGrants || script.grant['GM_log']) {
       lazy(sandbox, "GM_log", function() {
+        const _console = GM_getConsoleFor(safeWisafeWin, chromeWin);
         if (Scriptish_prefRoot.getValue("logToErrorConsole")) {
           let logger = new GM_ScriptLogger(script);
           return function() {
             const args = Array.slice(arguments);
             logger.log(args.join(" "));
-            sandbox.console.log.apply(sandbox.console, args);
+            _console.log.apply(_console, args);
           }
         }
-        return sandbox.console.log.bind(sandbox.console);
+        return _console.log.bind(_console);
       });
     }
 

extension/modules/api/GM_console.js
@@ -1,5 +1,5 @@
 "use strict";
-var EXPORTED_SYMBOLS = ["GM_console"];
+var EXPORTED_SYMBOLS = ["GM_console", "GM_getConsoleFor"];
 
 const Cu = Components.utils;
 Cu.import("resource://scriptish/logging.js");
@@ -15,7 +15,7 @@ const aux_functions = ["assert", "dir", "dirxml", "group", "groupEnd", "time",
                         "timeEnd", "count", "profile", "profileEnd"
                         ];
 
-function getConsoleFor(contentWindow, chromeWindow) {
+function GM_getConsoleFor(contentWindow, chromeWindow) {
   // we dont have a chromeWindow for e10s atm, see Scriptish_injectScripts impl
   if (chromeWindow) {
     let (rv = Scriptish_getFirebugConsole(contentWindow, chromeWindow)) {
@@ -33,7 +33,7 @@ function getConsoleFor(contentWindow, chromeWindow) {
 }
 
 function GM_console_legacy(script, contentWindow, chromeWindow) {
-  const _console = getConsoleFor(contentWindow, chromeWindow);
+  const _console = GM_getConsoleFor(contentWindow, chromeWindow);
   const console = { __exposedProps__: {__noSuchMethod__: "r"} };
   const prefix = "[" + (script.id || "Scriptish") + "]";
 
@@ -92,7 +92,7 @@ function GM_console(sandbox, script, contentWindow, chromeWindow) {
     return GM_console_legacy(script, contentWindow, chromeWindow);
   }
 
-  const _console = getConsoleFor(contentWindow, chromeWindow);
+  const _console = GM_getConsoleFor(contentWindow, chromeWindow);
   const console = Cu.createObjectIn(sandbox, {defineAs: "console"});
   const prefix = "[" + (script.id || "Scriptish") + "]";
 

commit ed76822231a235334031e976abe23051afad0449

Fix typo in typo introduced in 3fcb12c28f3b89a1ec17da3675f8a7ec41221604

extension/modules/utils/Scriptish_injectScripts.js
@@ -93,7 +93,7 @@ function Scriptish_injectScripts(options) {
 
     if (!useGrants || script.grant['GM_log']) {
       lazy(sandbox, "GM_log", function() {
-        const _console = GM_getConsoleFor(safeWisafeWin, chromeWin);
+        const _console = GM_getConsoleFor(safeWin, chromeWin);
         if (Scriptish_prefRoot.getValue("logToErrorConsole")) {
           let logger = new GM_ScriptLogger(script);
           return function() {